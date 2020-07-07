2acf623fb930c3a6a504017b8a27294d149820b3
gracemei65 <gracemei65@yahoo.com>
2020-07-07 02:13:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# spring-security-jwt
+
+simple springboot application used to build docker image and push to docker hub 
+
+contact : gracemei65@yahoo.com
---
+++ src/test/java/com/javatechie/springbootdocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.javatechie.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
